7d915998d34089c3d03fe2b9c927b030e2209b90
twosunaz <twosunazofficial@gmail.com>
2025-04-11 16:02:33 -0700

    installed bcryptjs as bcrypt doesn't work

--- packages/server/src/controllers/auth/index.ts
+++ packages/server/src/controllers/auth/index.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import { getRunningExpressApp } from '../../utils/getRunningExpressApp'
 import { User } from '../../database/entities/User'
 
