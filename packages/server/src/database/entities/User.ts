03250f59ee051cb5cc6abd47583a8b3106b750d7
twosunaz <twosunazofficial@gmail.com>
2025-04-12 15:50:30 -0700

    renamed User table in entities to Users

--- packages/server/src/database/entities/User.ts
+++ packages/server/src/database/entities/User.ts
@@ -1,6 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm'
 
-@Entity({ name: 'user' })
+@Entity({ name: 'Users' })
 export class User {
     @PrimaryGeneratedColumn('uuid')
     id: string
