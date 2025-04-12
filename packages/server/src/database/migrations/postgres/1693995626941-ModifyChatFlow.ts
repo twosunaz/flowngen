b4bc312c3f0130ea9f72eadf5d8141e5bc443b18
twosunaz <twosunazofficial@gmail.com>
2025-04-11 23:56:57 -0700

    refactored chatflow Migrations

--- packages/server/src/database/migrations/postgres/1693995626941-ModifyChatFlow.ts
+++ packages/server/src/database/migrations/postgres/1693995626941-ModifyChatFlow.ts
@@ -2,10 +2,24 @@ import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class ModifyChatFlow1693995626941 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "chat_flow" ALTER COLUMN "chatbotConfig" TYPE TEXT;`)
+        await queryRunner.query(`
+            DO $$
+            BEGIN
+                IF NOT EXISTS (
+                    SELECT 1
+                    FROM information_schema.columns
+                    WHERE table_name='chat_flow'
+                    AND column_name='chatbotConfig'
+                ) THEN
+                    ALTER TABLE "chat_flow" ADD COLUMN "chatbotConfig" TEXT;
+                END IF;
+            END$$;
+        `)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "chat_flow" ALTER COLUMN "chatbotConfig" TYPE VARCHAR;`)
+        await queryRunner.query(`
+            ALTER TABLE "chat_flow" DROP COLUMN IF EXISTS "chatbotConfig";
+        `)
     }
 }
